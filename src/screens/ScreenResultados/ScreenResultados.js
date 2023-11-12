c383cbba2342cb22e9c3d9cec4555eaf51357d8c
smignaquy <smignaquy@ydesa.edu.ar>
2023-11-12 18:38:41 -0300

    screen reultados

--- src/screens/ScreenResultados/ScreenResultados.js
+++ src/screens/ScreenResultados/ScreenResultados.js
@@ -6,13 +6,38 @@ class ScreenResultados extends Component {
     constructor() {
         super();
         this.state = {
+            usuarios: []
         };
     }
+
+    // componentDidMount() {
+    //     db.collection('users').where("owner", "==", this.props.).onSnapshot( me falta la parte de las props con que comparar el owner
+    //         docs => {
+    //             let users = []
+    //             docs.forEach(doc => {
+    //                 users.push(doc.data())
+    //             })
+
+    //             this.setState({
+    //                 usuarios: users,
+    //             })
+    //         }
+    //     )
+    // }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text>Estos son tus resultados de busqueda</Text>
+                {this.state.usuarios.length == 0 ? <ActivityIndicator size='large' color='blue' />
+                    :
+                    <FlatList 
+                        style={styles.flatlist}
+                        data= {this.state.usuarios}
+                        keyExtractor={ doc => doc.id.toString() }
+                        renderItem={ ({item}) => <Text> {item.owner} </Text> }
+                    />
+                }
             </View>
         );
     }
